77c6da274135ffb41516b38b98ae70ff07f32bbb
Jaslin Occius <ingjassage@gmail.com>
2025-10-17 10:43:47 -0400

    test

--- gestion-backend/dist/prisma.js
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from "../generated/prisma"; // Adjust the import path as necessary
-const prisma = new PrismaClient();
-export default prisma;
-//# sourceMappingURL=prisma.js.map
