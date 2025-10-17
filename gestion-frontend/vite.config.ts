a97ec34758a6969bf6866110d3364665eba2e3a0
Jaslin Occius <ingjassage@gmail.com>
2025-10-17 16:50:32 -0400

    Fix assets paths for GitHub Pages

--- gestion-frontend/vite.config.ts
+++ gestion-frontend/vite.config.ts
@@ -23,7 +23,7 @@ import { fileURLToPath } from "url";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ou '/' si c'est le repo principal
+  base: "./app-ujeph/gestion-frontend", // ou '/' si c'est le repo principal
   build: {
     outDir: "dist",
   },
