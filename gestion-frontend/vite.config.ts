c276ce33375a2d2cf27cd0e0d155704dce9b5998
Jaslin Occius <ingjassage@gmail.com>
2025-10-18 12:56:43 -0400

    Deploy clean version

--- gestion-frontend/vite.config.ts
+++ gestion-frontend/vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  base: "./", // ← DOIT ÊTRE "./"
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
   build: {
     outDir: "dist",
   },
@@ -20,4 +13,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
